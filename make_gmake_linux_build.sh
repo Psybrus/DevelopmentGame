52b192038cf901134c36b0e290ceac050c1df4df
Neil Richardson <neilo@neilo.gd>
2014-11-04 04:13:08 +1100

    generate with other toolchains

--- make_gmake_linux_build.sh
+++ make_gmake_linux_build.sh
@@ -2,4 +2,7 @@
 
 python Psybrus/reflection_parse.py DevelopmentGame
 
-Psybrus/Tools/genie/genie-linux --os=linux --platform=x64 gmake
+Psybrus/Tools/genie/genie-linux --os=linux --platform=x64 --toolchain=linux-clang gmake
+Psybrus/Tools/genie/genie-linux --os=linux --platform=x64 --toolchain=linux-gcc gmake
+Psybrus/Tools/genie/genie-linux --os=linux --toolchain=asmjs gmake
+
