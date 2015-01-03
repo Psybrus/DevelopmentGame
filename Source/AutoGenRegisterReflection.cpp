55d5041d6cd05378280c0dfd0f25fe168e6030b3
Neil Richardson <neilo@neilo.gd>
2015-01-03 21:43:15 +1100

    Sorted autogen reflection

--- Source/AutoGenRegisterReflection.cpp
+++ Source/AutoGenRegisterReflection.cpp
@@ -1,167 +1,167 @@
-#include "System/Content/CsTypes.h"
-#include "System/Content/CsResource.h"
-#include "System/Content/CsResourceImporter.h"
-#include "System/Content/CsRedirector.h"
-#include "System/Content/CsPackage.h"
-#include "System/Content/CsPackageImporter.h"
-#include "System/SysKernel.h"
-#include "System/Renderer/RsRenderState.h"
-#include "System/Renderer/RsCoreImpl.h"
-#include "System/Renderer/RsTypes.h"
-#include "System/Renderer/RsCore.h"
-#include "System/Renderer/RsSamplerState.h"
-#include "System/Scene/Import/ScnShaderImport.h"
-#include "System/Scene/Import/ScnSoundImport.h"
-#include "System/Scene/Import/ScnEntityImport.h"
-#include "System/Scene/Import/ScnComponentImport.h"
-#include "System/Scene/Import/ScnTextureImport.h"
-#include "System/Scene/Import/ScnAnimationImport.h"
-#include "System/Scene/Import/ScnModelImport.h"
-#include "System/Scene/Import/ScnMaterialImport.h"
-#include "System/Scene/Import/ScnFontImport.h"
-#include "System/Scene/ScnSpatialComponent.h"
-#include "System/Scene/Physics/ScnPhysicsRigidBodyComponent.h"
-#include "System/Scene/Physics/ScnPhysicsCollisionShape.h"
-#include "System/Scene/Physics/ScnPhysicsBoxCollisionShape.h"
-#include "System/Scene/Physics/ScnPhysicsWorldComponent.h"
-#include "System/Scene/Rendering/ScnParticleSystemComponent.h"
-#include "System/Scene/Rendering/ScnSpriteComponent.h"
-#include "System/Scene/Rendering/ScnRenderPipeline.h"
-#include "System/Scene/Rendering/ScnDebugRenderComponent.h"
-#include "System/Scene/Rendering/ScnModel.h"
-#include "System/Scene/Rendering/ScnShader.h"
-#include "System/Scene/Rendering/ScnViewComponent.h"
-#include "System/Scene/Rendering/ScnRenderableComponent.h"
-#include "System/Scene/Rendering/ScnTexture.h"
-#include "System/Scene/Rendering/ScnLightComponent.h"
-#include "System/Scene/Rendering/ScnRenderTarget.h"
-#include "System/Scene/Rendering/ScnFont.h"
-#include "System/Scene/Rendering/ScnMaterial.h"
-#include "System/Scene/Rendering/ScnShaderFileData.h"
-#include "System/Scene/Rendering/ScnTextureAtlas.h"
-#include "System/Scene/Rendering/ScnCanvasComponent.h"
-#include "System/Scene/Animation/ScnAnimationPose.h"
-#include "System/Scene/Animation/ScnAnimationTreeNode.h"
-#include "System/Scene/Animation/ScnAnimation.h"
-#include "System/Scene/Animation/ScnAnimationComponent.h"
-#include "System/Scene/Animation/ScnAnimationTreeBlendNode.h"
-#include "System/Scene/Animation/ScnAnimationTreeTrackNode.h"
-#include "System/Scene/Sound/ScnSoundListenerComponent.h"
-#include "System/Scene/Sound/ScnSound.h"
-#include "System/Scene/Sound/ScnSoundEmitter.h"
-#include "System/Scene/ScnEntity.h"
-#include "System/Scene/ScnComponent.h"
-#include "System/SysSystem.h"
-#include "System/Sound/SoLoud/SsCoreImplSoLoud.h"
-#include "System/Sound/SsCore.h"
-#include "System/File/FsTypes.h"
 #include "Math/MaAABB.h"
-#include "Math/MaPlane.h"
-#include "Math/MaMat3d.h"
-#include "Math/MaCPUVec4d.h"
 #include "Math/MaCPUVec2d.h"
 #include "Math/MaCPUVec3d.h"
+#include "Math/MaCPUVec4d.h"
+#include "Math/MaMat3d.h"
 #include "Math/MaMat4d.h"
+#include "Math/MaPlane.h"
 #include "Math/MaQuat.h"
-#include "GaTestModelComponent.h"
-#include "GaLevelComponent.h"
-#include "GaTestShaderComponent.h"
+#include "System/Content/CsPackage.h"
+#include "System/Content/CsPackageImporter.h"
+#include "System/Content/CsRedirector.h"
+#include "System/Content/CsResource.h"
+#include "System/Content/CsResourceImporter.h"
+#include "System/Content/CsTypes.h"
+#include "System/File/FsTypes.h"
+#include "System/Renderer/RsCore.h"
+#include "System/Renderer/RsCoreImpl.h"
+#include "System/Renderer/RsRenderState.h"
+#include "System/Renderer/RsSamplerState.h"
+#include "System/Renderer/RsTypes.h"
+#include "System/Scene/Animation/ScnAnimation.h"
+#include "System/Scene/Animation/ScnAnimationComponent.h"
+#include "System/Scene/Animation/ScnAnimationPose.h"
+#include "System/Scene/Animation/ScnAnimationTreeBlendNode.h"
+#include "System/Scene/Animation/ScnAnimationTreeNode.h"
+#include "System/Scene/Animation/ScnAnimationTreeTrackNode.h"
+#include "System/Scene/Import/ScnAnimationImport.h"
+#include "System/Scene/Import/ScnComponentImport.h"
+#include "System/Scene/Import/ScnEntityImport.h"
+#include "System/Scene/Import/ScnFontImport.h"
+#include "System/Scene/Import/ScnMaterialImport.h"
+#include "System/Scene/Import/ScnModelImport.h"
+#include "System/Scene/Import/ScnShaderImport.h"
+#include "System/Scene/Import/ScnSoundImport.h"
+#include "System/Scene/Import/ScnTextureImport.h"
+#include "System/Scene/Physics/ScnPhysicsBoxCollisionShape.h"
+#include "System/Scene/Physics/ScnPhysicsCollisionShape.h"
+#include "System/Scene/Physics/ScnPhysicsRigidBodyComponent.h"
+#include "System/Scene/Physics/ScnPhysicsWorldComponent.h"
+#include "System/Scene/Rendering/ScnCanvasComponent.h"
+#include "System/Scene/Rendering/ScnDebugRenderComponent.h"
+#include "System/Scene/Rendering/ScnFont.h"
+#include "System/Scene/Rendering/ScnLightComponent.h"
+#include "System/Scene/Rendering/ScnMaterial.h"
+#include "System/Scene/Rendering/ScnModel.h"
+#include "System/Scene/Rendering/ScnParticleSystemComponent.h"
+#include "System/Scene/Rendering/ScnRenderPipeline.h"
+#include "System/Scene/Rendering/ScnRenderTarget.h"
+#include "System/Scene/Rendering/ScnRenderableComponent.h"
+#include "System/Scene/Rendering/ScnShader.h"
+#include "System/Scene/Rendering/ScnShaderFileData.h"
+#include "System/Scene/Rendering/ScnSpriteComponent.h"
+#include "System/Scene/Rendering/ScnTexture.h"
+#include "System/Scene/Rendering/ScnTextureAtlas.h"
+#include "System/Scene/Rendering/ScnViewComponent.h"
+#include "System/Scene/ScnComponent.h"
+#include "System/Scene/ScnEntity.h"
+#include "System/Scene/ScnSpatialComponent.h"
+#include "System/Scene/Sound/ScnSound.h"
+#include "System/Scene/Sound/ScnSoundEmitter.h"
+#include "System/Scene/Sound/ScnSoundListenerComponent.h"
+#include "System/Sound/SoLoud/SsCoreImplSoLoud.h"
+#include "System/Sound/SsCore.h"
+#include "System/SysKernel.h"
+#include "System/SysSystem.h"
 #include "GaAnimationControllerComponent.h"
-#include "GaTestFontComponent.h"
 #include "GaCameraComponent.h"
-#include "GaTestSelectionComponent.h"
+#include "GaLevelComponent.h"
+#include "GaTestFontComponent.h"
+#include "GaTestModelComponent.h"
 #include "GaTestParticleComponent.h"
+#include "GaTestSelectionComponent.h"
+#include "GaTestShaderComponent.h"
 void AutoGenRegisterReflection()
 {
-	CsDependency::StaticRegisterClass();
+	MaAABB::StaticRegisterClass();
+	MaCPUVec2d::StaticRegisterClass();
+	MaCPUVec3d::StaticRegisterClass();
+	MaCPUVec4d::StaticRegisterClass();
+	MaMat3d::StaticRegisterClass();
+	MaMat4d::StaticRegisterClass();
+	MaPlane::StaticRegisterClass();
+	MaQuat::StaticRegisterClass();
+	CsPackage::StaticRegisterClass();
+	CsPackageDependencies::StaticRegisterClass();
+	CsRedirectorImport::StaticRegisterClass();
+	CsRedirector::StaticRegisterClass();
 	CsResource::StaticRegisterClass();
 	CsResourceImporterAttribute::StaticRegisterClass();
 	CsResourceImporter::StaticRegisterClass();
-	CsRedirectorImport::StaticRegisterClass();
-	CsRedirector::StaticRegisterClass();
-	CsPackage::StaticRegisterClass();
-	CsPackageDependencies::StaticRegisterClass();
-	SysKernel::StaticRegisterClass();
+	CsDependency::StaticRegisterClass();
+	FsTimestamp::StaticRegisterClass();
+	FsStats::StaticRegisterClass();
+	RsCore::StaticRegisterClass();
+	RsCoreImpl::StaticRegisterClass();
 	RsRenderTargetBlendState::StaticRegisterClass();
 	RsBlendState::StaticRegisterClass();
 	RsStencilFaceState::StaticRegisterClass();
 	RsDepthStencilState::StaticRegisterClass();
 	RsRasteriserState::StaticRegisterClass();
 	RsRenderStateDesc::StaticRegisterClass();
-	RsCoreImpl::StaticRegisterClass();
-	RsColour::StaticRegisterClass();
-	RsCore::StaticRegisterClass();
 	RsSamplerStateDesc::StaticRegisterClass();
+	RsColour::StaticRegisterClass();
+	ScnAnimation::StaticRegisterClass();
+	ScnAnimationComponent::StaticRegisterClass();
+	ScnAnimationPose::StaticRegisterClass();
+	ScnAnimationTreeBlendNode::StaticRegisterClass();
+	ScnAnimationTreeNode::StaticRegisterClass();
+	ScnAnimationTreeTrackNode::StaticRegisterClass();
+	ScnAnimationImport::StaticRegisterClass();
+	ScnComponentImport::StaticRegisterClass();
+	ScnEntityImport::StaticRegisterClass();
+	ScnFontImport::StaticRegisterClass();
+	ScnMaterialImport::StaticRegisterClass();
+	ScnModelImport::StaticRegisterClass();
 	ScnShaderImport::StaticRegisterClass();
 	ScnSoundImport::StaticRegisterClass();
-	ScnEntityImport::StaticRegisterClass();
-	ScnComponentImport::StaticRegisterClass();
 	ScnTextureImport::StaticRegisterClass();
-	ScnAnimationImport::StaticRegisterClass();
-	ScnModelImport::StaticRegisterClass();
-	ScnMaterialImport::StaticRegisterClass();
-	ScnFontImport::StaticRegisterClass();
-	ScnSpatialComponent::StaticRegisterClass();
-	ScnPhysicsRigidBodyComponent::StaticRegisterClass();
-	ScnPhysicsCollisionShape::StaticRegisterClass();
 	ScnPhysicsBoxCollisionShape::StaticRegisterClass();
+	ScnPhysicsCollisionShape::StaticRegisterClass();
+	ScnPhysicsRigidBodyComponent::StaticRegisterClass();
 	ScnPhysicsWorldComponent::StaticRegisterClass();
-	ScnParticleSystemComponent::StaticRegisterClass();
-	ScnSpriteComponent::StaticRegisterClass();
-	ScnRenderPipeline::StaticRegisterClass();
+	ScnCanvasComponent::StaticRegisterClass();
 	ScnDebugRenderComponent::StaticRegisterClass();
-	ScnModel::StaticRegisterClass();
-	ScnModelComponent::StaticRegisterClass();
-	ScnShader::StaticRegisterClass();
-	ScnViewComponent::StaticRegisterClass();
-	ScnRenderableComponent::StaticRegisterClass();
-	ScnTexture::StaticRegisterClass();
-	ScnLightComponent::StaticRegisterClass();
-	ScnRenderTarget::StaticRegisterClass();
 	ScnFont::StaticRegisterClass();
 	ScnFontUniformBlockData::StaticRegisterClass();
 	ScnFontDrawParams::StaticRegisterClass();
 	ScnFontComponent::StaticRegisterClass();
+	ScnLightComponent::StaticRegisterClass();
 	ScnMaterial::StaticRegisterClass();
 	ScnMaterialComponent::StaticRegisterClass();
+	ScnModel::StaticRegisterClass();
+	ScnModelComponent::StaticRegisterClass();
+	ScnParticleSystemComponent::StaticRegisterClass();
+	ScnRenderPipeline::StaticRegisterClass();
+	ScnRenderTarget::StaticRegisterClass();
+	ScnRenderableComponent::StaticRegisterClass();
+	ScnShader::StaticRegisterClass();
 	ScnShaderViewUniformBlockData::StaticRegisterClass();
 	ScnShaderLightUniformBlockData::StaticRegisterClass();
 	ScnShaderObjectUniformBlockData::StaticRegisterClass();
 	ScnShaderBoneUniformBlockData::StaticRegisterClass();
 	ScnShaderAlphaTestUniformBlockData::StaticRegisterClass();
+	ScnSpriteComponent::StaticRegisterClass();
+	ScnTexture::StaticRegisterClass();
 	ScnTextureAtlas::StaticRegisterClass();
-	ScnCanvasComponent::StaticRegisterClass();
-	ScnAnimationPose::StaticRegisterClass();
-	ScnAnimationTreeNode::StaticRegisterClass();
-	ScnAnimation::StaticRegisterClass();
-	ScnAnimationComponent::StaticRegisterClass();
-	ScnAnimationTreeBlendNode::StaticRegisterClass();
-	ScnAnimationTreeTrackNode::StaticRegisterClass();
-	ScnSoundListenerComponent::StaticRegisterClass();
-	ScnSound::StaticRegisterClass();
-	ScnSoundEmitterComponent::StaticRegisterClass();
-	ScnEntity::StaticRegisterClass();
+	ScnViewComponent::StaticRegisterClass();
 	ScnComponentAttribute::StaticRegisterClass();
 	ScnComponent::StaticRegisterClass();
-	SysSystem::StaticRegisterClass();
+	ScnEntity::StaticRegisterClass();
+	ScnSpatialComponent::StaticRegisterClass();
+	ScnSound::StaticRegisterClass();
+	ScnSoundEmitterComponent::StaticRegisterClass();
+	ScnSoundListenerComponent::StaticRegisterClass();
 	SsCoreImplSoLoud::StaticRegisterClass();
 	SsCore::StaticRegisterClass();
-	FsTimestamp::StaticRegisterClass();
-	FsStats::StaticRegisterClass();
-	MaAABB::StaticRegisterClass();
-	MaPlane::StaticRegisterClass();
-	MaMat3d::StaticRegisterClass();
-	MaCPUVec4d::StaticRegisterClass();
-	MaCPUVec2d::StaticRegisterClass();
-	MaCPUVec3d::StaticRegisterClass();
-	MaMat4d::StaticRegisterClass();
-	MaQuat::StaticRegisterClass();
-	GaTestModelComponent::StaticRegisterClass();
-	GaLevelComponent::StaticRegisterClass();
-	GaTestShaderComponent::StaticRegisterClass();
+	SysKernel::StaticRegisterClass();
+	SysSystem::StaticRegisterClass();
 	GaAnimationControllerComponent::StaticRegisterClass();
-	GaTestFontComponent::StaticRegisterClass();
 	GaCameraComponent::StaticRegisterClass();
-	GaTestSelectionComponent::StaticRegisterClass();
+	GaLevelComponent::StaticRegisterClass();
+	GaTestFontComponent::StaticRegisterClass();
+	GaTestModelComponent::StaticRegisterClass();
 	GaTestParticleComponent::StaticRegisterClass();
+	GaTestSelectionComponent::StaticRegisterClass();
+	GaTestShaderComponent::StaticRegisterClass();
 }
